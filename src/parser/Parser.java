75b6a06ef3ba592725996889fc670a9266b28dce
root <root@00fcee48-bca6-446a-b9d5-5d3b22c4d7dc>
2012-11-16 13:26:18 +0000

    better?
    git-svn-id: svn://127.0.0.1/var/cvs/xu/IniParser/trunk@175 00fcee48-bca6-446a-b9d5-5d3b22c4d7dc

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -12,106 +12,98 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Parser {
-	
-	List<String> text;
-	Map<String,String> paramsMap;
-	
+
+	private Map<String, String> paramsMap;
+
 	public static void main(String[] args) throws IOException {
-		
+
 		Parser p = new Parser();
-		
+
 		p.load("e:\\MyDocuments\\My\\tm\\test.ini");
-		
-		
-		String s = p.get("parameter3");
+		p.append("e:\\MyDocuments\\My\\tm\\test1.ini");
+
+		for (Map.Entry<String, String> e : p.paramsMap.entrySet()) {
+			System.out.println(e.getKey() + "&" + e.getValue());
+		}
+
+		String s = p.get("parameter1");
 		System.out.println("result");
 		System.out.println(s);
-		
-		
-//		Формат ini:
-//
-//		parameter1=value1
-//		parameter2=value2 ;comment
-//		;comment
-//
-//		parameter3=val=ue3
-//
+
+		// Формат ini:
+		//
+		// parameter1=value1
+		// parameter2=value2 ;comment
+		// ;comment
+		//
+		// parameter3=val=ue3
+		//
 
 	}
-	
-	public void load(String fileName) throws IOException { 
-		text = new LinkedList<String>();
-		BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fileName))));
-		String s = "";
 
-		while ((s = r.readLine()) != null) {
-			text.add(s);
-		}
-		
-		paramsMap = parseParams(text);
-		
-		r.close();
+	public void load(String fileName) throws IOException {
+
+		paramsMap = new TreeMap<String, String>();
+
+		append(fileName);
 	}
-	
-	
+
 	public void append(String fileName) throws IOException {
-		BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fileName))));
-		String s = "";
-		List<String> textToAppend = new LinkedList<String>();
-		while ((s = r.readLine()) != null) {
-			textToAppend.add(s);
+		BufferedReader r = null;
+
+		try {
+			r = new BufferedReader(new InputStreamReader(new FileInputStream(
+					new File(fileName))));
+
+			String s = "";
+			List<String> text = new LinkedList<String>();
+			while ((s = r.readLine()) != null) {
+				text.add(s);
+			}
+			parseParams(text);
 		}
-		paramsMap.putAll(parseParams(text));
-		
-//	m.b. here'e better to do so: (no extra List will be created)
-		
-//		while ((s = r.readLine()) != null) {
-//		text.add(s);
-//	}
-//	
-//	paramsMap = parseParams(text);
-		
-		r.close();
+
+		finally {
+			if (r != null) {
+				try {
+					r.close();
+				} catch (IOException e) {
+				}
+			}
+		}
 	}
-	
-	public String get(String name){
+
+	public String get(String name) {
 		return paramsMap.get(name);
 	}
-	
-	private Map<String,String> parseParams(List<String> text) {
-		Map<String,String> paramsMap = new TreeMap<String,String>();
-		for (String s: text) {
+
+	private void parseParams(List<String> text) {
+
+		for (String s : text) {
 			s = cutComments(s);
-			
+
 			if (s.trim().length() == 0) {
 				continue;
 			}
-			
-			if (s.indexOf('=') == -1) {
-				throw new IllegalArgumentException("Invalid parameter found: " + s);
+
+			int separatorIndex = s.indexOf('=');
+			if (separatorIndex == -1) {
+				throw new IllegalArgumentException("Invalid parameter found: "
+						+ s);
 			}
-			
-			paramsMap.put(s.substring(0,s.indexOf('=')).trim(), 
-					s.substring(s.indexOf('=') + 1).trim());
-			
-			
+
+			paramsMap.put(s.substring(0, separatorIndex).trim(),
+					s.substring(separatorIndex + 1).trim());
+
 		}
-	
-		for (Map.Entry<String, String> e : paramsMap.entrySet()) {
-			System.out.println(e.getKey() + "&" + e.getValue());
-		}
-		
-		return paramsMap;
-	} 
-	
+	}
+
 	private String cutComments(String s) {
 		if (s.indexOf(';') == -1) {
 			return s;
 		}
 		return s.substring(0, s.indexOf(';'));
-		
+
 	}
-	
-	
 
 }
